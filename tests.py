9e7c26f343dcbd9608e92063704b5b042894ee71
Sergio Tabarini <tabarinisergio@gmail.com>
2015-01-22 09:52:58 -0600

    Added testing to function 'has numbers()'

--- tests.py
+++ tests.py
@@ -1,15 +1,22 @@
 import os
 import nose
 import unittest
+from application import ProgramControl
+
 
 APPLICATION_DIR = 'application.py'
 
 class TestApplication(unittest.TestCase):
+	
+	def test_has_numbers(self):
+		Test = ProgramControl()
+		assert (Test.has_numbers('Hola') == False)
+		assert (Test.has_numbers('H1') == True)
+		
+	def test_lint(self):
+		resultado = os.system('pylint' + " " +APPLICATION_DIR)
+		return resultado
 
-
-    def test_lint(self):
-        resultado = os.system('pylint' + " " +APPLICATION_DIR)
-        return resultado
 
 if __name__ == '__main__':
     unittest.main()
